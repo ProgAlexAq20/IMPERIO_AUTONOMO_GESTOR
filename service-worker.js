45ff3115a9feb2e7a74dc89085d7031dce21bda8
Alexandre <aquino.alexandre08@gmail.com>
2026-04-17 17:34:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,42 @@
-self.addEventListener("install", (e) => {
-  self.skipWaiting();
+const CACHE_NAME = "imperio-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALAÇÃO
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// ATIVAÇÃO (limpa cache antigo)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (offline first)
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
